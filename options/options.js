410ae2b4460d7b04a353d8c84eafec2541d7006e
Crowdbotics <team@crowdbotics.com>
2023-09-12 16:30:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "floral-firefly-43357",
+  url: "https://floral-firefly-43357.botics.co",
+  api: "https://floral-firefly-43357.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
